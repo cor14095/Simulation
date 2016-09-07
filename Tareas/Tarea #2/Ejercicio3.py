8937bd29e0356c9b830577aa07b91029eb5500ad
Alejandro Cortes <ale.crts1996@gmail.com>
2016-09-07 00:04:08 +0000

    Fixed the 'Ejercicio3.py' in 'Tarea #2' to correctly calculate the probability.

--- Tareas/Tarea #2/Ejercicio3.py
+++ Tareas/Tarea #2/Ejercicio3.py
@@ -25,19 +25,19 @@ for i in range(1, n):
 		if (years==0):
 			flujoDeCaja += -800
 		elif (years==1):
-			flujoDeCaja += (random.normalvariate(-800, 50) / (1 + 0.1)**years)-800
+			flujoDeCaja += (random.normalvariate(-800, 50) / (1 + 0.1)**years)
 		elif (years==2):
-			flujoDeCaja += (random.normalvariate(-800, 100) / (1 + 0.1)**years)-800
+			flujoDeCaja += (random.normalvariate(-800, 100) / (1 + 0.1)**years)
 		elif (years==3):
-			flujoDeCaja += (random.normalvariate(-700, 150) / (1 + 0.1)**years)-800
+			flujoDeCaja += (random.normalvariate(-700, 150) / (1 + 0.1)**years)
 		elif (years==4):
-			flujoDeCaja += (random.normalvariate(300, 200) / (1 + 0.1)**years)-800
+			flujoDeCaja += (random.normalvariate(300, 200) / (1 + 0.1)**years)
 		elif (years==5):
-			flujoDeCaja += (random.normalvariate(400, 200) / (1 + 0.1)**years)-800
+			flujoDeCaja += (random.normalvariate(400, 200) / (1 + 0.1)**years)
 		elif (years==6):
-			flujoDeCaja += (random.normalvariate(500, 200) / (1 + 0.1)**years)-800
+			flujoDeCaja += (random.normalvariate(500, 200) / (1 + 0.1)**years)
 		elif (years==7):
-			flujoDeCaja += (random.uniform(200, 8440) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.uniform(200, 8440) / (1 + 0.1)**years)
 			listaCajasHotel.append(flujoDeCaja)
 			flujoDeCaja=0
 		years+=1
@@ -50,21 +50,21 @@ for i in range(1, n):
     # Iteramos...
     while (years < 8):
 		if (years==0):
-			flujoDeCaja += -800
+			flujoDeCaja += -900
 		elif (years==1):
-			flujoDeCaja += (random.normalvariate(-600, 50) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.normalvariate(-600, 50) / (1 + 0.1)**years)
 		elif (years==2):
-			flujoDeCaja += (random.normalvariate(-200, 50) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.normalvariate(-200, 50) / (1 + 0.1)**years)
 		elif (years==3):
-			flujoDeCaja += (random.normalvariate(-600, 100) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.normalvariate(-600, 100) / (1 + 0.1)**years)
 		elif (years==4):
-			flujoDeCaja += (random.normalvariate(250, 150) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.normalvariate(250, 150) / (1 + 0.1)**years)
 		elif (years==5):
-			flujoDeCaja += (random.normalvariate(350, 150) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.normalvariate(350, 150) / (1 + 0.1)**years)
 		elif (years==6):
-			flujoDeCaja += (random.normalvariate(400, 150) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.normalvariate(400, 150) / (1 + 0.1)**years)
 		elif (years==7):
-			flujoDeCaja += (random.uniform(1600, 6000) / (1 + 0.1)**years) - 800
+			flujoDeCaja += (random.uniform(1600, 6000) / (1 + 0.1)**years)
 			listaCajasCC.append(flujoDeCaja)
 			flujoDeCaja=0
 		years+=1
@@ -74,8 +74,8 @@ for i in range(1, n):
 	reporteHotel += listaCajasHotel[i-1]
 	reporteCC    += listaCajasCC[i-1]
 
-reporteHotel /= n
-reporteCC /= n
+reporteHotel /= (n)
+reporteCC /= (n)
 
 print "    __ Reporte de simulacion __ "
 print " Para: " + str(n) + " iteraciones."
